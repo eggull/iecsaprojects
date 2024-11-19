d3dc6e7df62627370b76b388ec925f7f89b0afe4
eggull <12147@soyunaq.mx>
2024-11-18 22:20:24 -0600

    second

--- FinalProject.c
+++ FinalProject.c
@@ -1,2 +1,3 @@
 /* This final project is for the asignature progamation, we scripted a algoritms from a minigame called "Mexwall"
-Si esto se actualiza ya chingamos*/
+Si esto se actualiza ya chingamos
+vale verga no se que mierda hice pero creo que funciono*/

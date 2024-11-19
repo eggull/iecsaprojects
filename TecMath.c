999f0c1e4d4168c195ad6a3595dfd885f0680b4e
eggull <12147@soyunaq.mx>
2024-11-18 22:53:28 -0600

    TecMath

---
+++ TecMath.c
@@ -0,0 +1,19 @@
+/* 
+Here, we will do the spanning tree and triangulation algorithm.
+
+In fact, first we have to do the graphs of the points with gotoxy or any library that allow us graph the triangulation and incenters of this triangles
+
+1- Get the points
+
+2- Plot the points
+
+3- Make the triangulation and get the incenters
+
+4- Make an algorithm that connects these incenters
+
+5- Graph the expantion tree
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>

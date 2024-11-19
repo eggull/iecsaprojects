621462403d130ccbfb8d5169b98d9ca0116de1c5
eggull <12147@soyunaq.mx>
2024-11-18 22:43:39 -0600

    FinalProject

--- FinalProject.c
+++ FinalProject.c
@@ -1,14 +1,17 @@
 /* This final project is for the asignature progamation, we scripted a algoritms from a minigame called "Mexwall"
 
-The game 
+The game has a fun theme where the character can move freely around the map but will constantly be hit by objects that can knock him down. If they touch him, he loses. 
+The speed of the objects and the time are proportional.
 
-1- Lets do a movement player with WASD W(UP, ANIMATION UP), A(LEFT, ANIMATION LEFT), S(DOWN, ANIMATION DOWN), D(RIGHT, ANIMATION RIGHT)
+1- Lets do the movement player with WASD W(UP, ANIMATION UP), A(LEFT, ANIMATION LEFT), S(DOWN, ANIMATION DOWN), D(RIGHT, ANIMATION RIGHT)
 
-2- 
+2- Lets create the objects, each object only have 1 direction of movement.
 
+3- Lets create the proportional speed of the objects with the time
 
 */
 
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
+    
